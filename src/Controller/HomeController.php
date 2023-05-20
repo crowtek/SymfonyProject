d58c57a6c677b633003254add2ea87dfd6527225
crowtek <prt3@hotmail.de>
2023-05-20 18:48:32 +0200

    Namen angepasst um übersicht zu behalten beim erstellen von meheren Seiten

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,16 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloWorldController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/hello/world', name: 'hello_world')]
+    #[Route('/home', name: 'home')]
     public function index(): Response
     {
         $title = "SYMFONY";
 
-        return $this->render('hello_world/index.html.twig', [
+        return $this->render('home/index.html.twig', [
             'title' => $title,
         ]);
     }
 }
-//http://localhost/HelloWorld/public/index.php/hello/world
